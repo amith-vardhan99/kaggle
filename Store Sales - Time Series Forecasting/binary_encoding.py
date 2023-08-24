21293a5b5284033cad614542647923377cf058c0
Amith Vardhan Reddy Surasani <amithvardhangd3@gmail.com>
2023-08-23 21:08:51 -0400

    .

--- Store Sales - Time Series Forecasting/binary_encoding.py
+++ Store Sales - Time Series Forecasting/binary_encoding.py
@@ -1,27 +1,26 @@
 import numpy as np
-import pandas as pd
-from math import *
 
-class EncodeBinary():
-    def bin_to_dec(self,num):
+
+class EncodeBinary:
+    def bin_to_dec(self, num):
         n = num
         st = []
         ctr = 0
-        while n>0:
+        while n > 0:
             r = int(n % 2)
             n = int(n / 2)
             st.append(r)
         fin = st[::-1]
         return fin
 
-    def unify_binary_length(self,arr):
+    def unify_binary_length(self, arr):
         length = []
         for i in arr:
             length.append(len(i))
         max_len = max(length)
         fin = []
         for i in arr:
-            if len(i)!=max_len:
+            if len(i) != max_len:
                 z = np.zeros(max_len-len(i))
                 z = z.astype("int")
                 t = list(z)
@@ -29,8 +28,11 @@ class EncodeBinary():
             else:
                 fin.append(i)
         return fin
-    def column_names(name,col_num):
-        lis = []
-        for i in range(0,col_num):
-            lis.append(name+"_"+str(col_num))
-        return lis
+
+    def column_names(self, nam, col_num):
+        li = []
+        for i in range(0, col_num):
+            h = nam+"_"+str(i)
+            li.append(h)
+        return li
+#%%
